6bf7e13578b9cc5c7c585c720adbfff6dae03b39
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-17 14:05:47 +0100

    Set up basic ring webserver

---
+++ src/ring_todo_list/core.clj
@@ -0,0 +1,10 @@
+(ns ring-todo-list.core
+  (:use ring.adapter.jetty))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
+
+(defn -main [& args]
+  (run-jetty handler {:port 3000}))
