073fbb54c398a82d3f9b4f9b6542ba8fb034ccb9
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-22 11:13:32 +0100

    add post method to the endpoint

--- src/ring_todo_list/core.clj
+++ src/ring_todo_list/core.clj
@@ -1,17 +1,31 @@
 (ns ring-todo-list.core
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [muuntaja.core :as m]
+            ))
 
 (def app
   (ring/ring-handler
     (ring/router
-      ["/api/hello" {:get {
-                        :handler
-                        (fn [request]
-                          {:status 200
-                           :headers {"Content-Type" "text/plain"}
-                           :body "Hello World"})
-                        }}])))
+      ["/api/hello" {:get  {
+                            :handler
+                            (fn [request]
+                              {:status  200
+                               :headers {"Content-Type" "text/plain"}
+                               :body    "Hello World"})
+                            }
+                     :post {
+                            :handler
+                            (fn [request]
+                              {:status  200
+                               :headers {"Content-Type" "text/plain"}
+                               :body    (str (:body-params request))})
+                            }}]
+      {:data {
+              :muuntaja m/instance
+              :middleware [muuntaja/format-middleware]
+              }})))
 
 (defn -main [& args]
   (run-jetty app {:port 3000}))
