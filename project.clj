40cfe680bbc80d7a958512d64877d4bdef82f9b2
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-22 12:05:59 +0100

    move from ring-core to ring so that you get the middleware to reload code changes

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.9.3"]
+                 [ring "1.9.3"]
                  [ring/ring-jetty-adapter "1.9.3"]
                  [metosin/reitit "0.5.13"]]
   :repl-options {:init-ns ring-todo-list.core}
