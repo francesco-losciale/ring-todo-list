ed19ce5829f99af7917337056d7283ceec52f48f
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-22 17:47:32 +0100

    add GET todo-list by id

--- src/ring_todo_list/core.clj
+++ src/ring_todo_list/core.clj
@@ -9,22 +9,33 @@
 (def app
   (ring/ring-handler
     (ring/router
-      [["/api/v1/todo-list"
-        {:post
+      [
+       ["/api/v1/todo-list"
+        [""
+         {:post
+          {:handler
+           (fn [{{:keys [todo-list]} :body-params}]         ;{{:keys [x y]} :body-params}
+             {:status 200
+              :body   todo-list
+              })
+           }
+          :get
+          {:handler
+           (fn [_]
+             {:status 200
+              :body   {:todo-list [{:id 2 :text "Do something"}]}
+              })
+           }
+          }]
+        ["/:id"
+         {:get
          {:handler
-          (fn [{{:keys [todo-list]} :body-params}]          ;{{:keys [x y]} :body-params}
+          (fn [{{:keys [id]} :path-params}]
             {:status 200
-             :body   todo-list
-             })
-          }
-         :get
-         {:handler
-          (fn [_]
-            {:status  200
-             :body    {:todo-list [{:id 2 :text "Do something"}]}
-             })
-          }
-         }]]
+             :body   {:id id :todo-list [{:id 2 :text "Do something else"}]}
+             })}}]
+        ]
+       ]
 
       {:data {
               :muuntaja   m/instance
