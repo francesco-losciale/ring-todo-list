0819a224231abef6dc198670f32e8f0a2ea71439
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-23 11:15:01 +0100

    add TODOs

--- src/ring_todo_list/db.clj
+++ src/ring_todo_list/db.clj
@@ -1,6 +1,10 @@
 (ns ring-todo-list.db
   (:require [monger.core :as mg]))
 
+; TODO
+; migration scripts for mongodb
+; create apis to read and save lists
+
 (defn connect []
   (let [conn (mg/connect {:host "localhost" :port 27017})
         db   (mg/get-db conn "todo-lists")]
